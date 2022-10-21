7161dd2f4c92f98749002a01cc75235ef6810e40
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-10-21 14:43:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ var download = document.querySelectorAll('.download li')
 
 // default properties for QR Code
 var properties = {
-        width: 300,
-        height: 300,
+        width: 250,
+        height: 250,
         type: "svg",
         margin: 10,
         data: '',
